cccb0b5249ffaddf4c27a4a72d6b8b8c3b71d05b
CarlEverest78 <thepythoneer@gmail.com>
2022-10-02 15:54:19 +0200

    add client folder to server root, add proxy to client package.json, add app.use static path to client build to express

--- app.js
+++ app.js
@@ -9,23 +9,14 @@ import cors from "cors"
 
 import path from "path"
 
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-const __dirname = dirname(fileURLToPath(import.meta.url));
+// import { dirname } from 'path';
+// import { fileURLToPath } from 'url';
+// const __dirname = dirname(fileURLToPath(import.meta.url));
      
-// let corsOptions = {             //=> this is very important when running react and express both on localhost
-//   origin: "*",
-//   optionsSuccessStatus: 200,
-// }
-// app.use(cors(corsOptions));
-
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-// app.get("/",(req,res)=>{
-//     res.send("hello")
-// })
 
 // app.use('/user', User)
 // app.use('/upload', Upload)
@@ -35,11 +26,6 @@ app.use(cors())
 // app.use('/update', Update)
 
 app.use(express.static(path.join(__dirname, './client/build')));
-
-// app.get('/*', function (req, res) {
-//   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-// });
-
 
 app.listen(process.env.PORT || 3001, (req,res)=>{
     console.log("Connected to server on port 3001")
