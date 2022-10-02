efb6cc5c8664b2bf8f2705c6cfcce0a1fe29c666
CarlEverest78 <thepythoneer@gmail.com>
2022-10-02 21:43:36 +0200

    add all original routes to express server

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 import express from "express"
 import cors from "cors"
-// import User from './routes/user.js'
-// import Upload from "./routes/upload.js"
-// import Posts from "./routes/posts.js"
-// import Like from "./routes/like.js"
-// import Delete from "./routes/delete.js"
-// import Update from "./routes/update.js"
+import User from './routes/user.js'
+import Upload from "./routes/upload.js"
+import Posts from "./routes/posts.js"
+import Like from "./routes/like.js"
+import Delete from "./routes/delete.js"
+import Update from "./routes/update.js"
 
 import path from "path"
 
@@ -17,17 +17,12 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.use(express.static(path.join(__dirname, './client/build')));
-
-app.get("/hello", (req,res)=>{
-    res.send("hello")
-})
-// app.use('/user', User)
-// app.use('/upload', Upload)
-// app.use('/posts', Posts)
-// app.use('/like', Like)
-// app.use('/delete', Delete)
-// app.use('/update', Update)
+app.use('/user', User)
+app.use('/upload', Upload)
+app.use('/posts', Posts)
+app.use('/like', Like)
+app.use('/delete', Delete)
+app.use('/update', Update)
 
 app.use(express.static(path.join(__dirname, './client/build')));
 
