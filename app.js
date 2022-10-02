21643ba91d24ca1eb0ece3dccd1d896707b7bed3
CarlEverest78 <thepythoneer@gmail.com>
2022-10-02 15:49:05 +0200

    add client folder to server root, add proxy to client package.json, add app.use static path to client build to express

--- app.js
+++ app.js
@@ -36,9 +36,9 @@ app.use(cors())
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+// app.get('/*', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+// });
 
 
 app.listen(process.env.PORT || 3001, (req,res)=>{
