f28ae86423419bd51d4d9171f89e7c90b4511402
CarlEverest78 <thepythoneer@gmail.com>
2022-10-03 09:35:34 +0200

    Try baseurl in template string

--- client/src/pages/Posts/Posts.js
+++ client/src/pages/Posts/Posts.js
@@ -7,7 +7,7 @@ import ScrollTop from '../../hooks/useScrollTop';
 export default function Posts({sort, filter}) {
   const [posts, setPosts] = React.useState([])
 
-  const baseURL = "" || 'https://localhost:3001'
+  const baseURL = 'https://localhost:3001'
   
   React.useEffect(()=> {
     ScrollTop()
