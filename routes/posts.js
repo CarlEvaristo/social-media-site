b54ef814c6e1ab838faa92a9e418329d3287a7b8
CarlEverest78 <thepythoneer@gmail.com>
2022-10-03 11:47:06 +0200

    Try baseurl in template string

--- routes/posts.js
+++ routes/posts.js
@@ -5,13 +5,13 @@ const router = express.Router()
 
 router.get("/", (req,res)=> {
     db.query("SELECT * FROM posts ", 
-    (err,result) => err ? res.status(err.status).send(err) : res.send(result))
+    (err,result) => err ? res.send(err) : res.send(result))
 })
 
 router.get("/:id", (req,res)=> {
     const id= req.params.id 
     db.query("SELECT * FROM posts WHERE id = ?", [id],
-    (err,result) => err ? res.send(err) : res.send(result))
+    (err,result) => err ? console.log(err) : res.send(result))
 })
 
 export default router
